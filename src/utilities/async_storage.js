62040996b2052ed38caffe35554c2b21e3adb08b
Mark Wong <markwong.digital@gmail.com>
2022-08-24 01:20:04 -0700

    Add async_storage methods

---
+++ src/utilities/async_storage.js
@@ -0,0 +1,37 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// add or update
+export const set = async (key, value) => {
+    console.log("SAVING TO ASYNCSTORAGE ::: ", key, value);
+
+    try {
+        const jsonValue = JSON.stringify(value);
+        return await AsyncStorage.setItem(key, jsonValue);
+    } catch (e) {
+        return { error: "Failed to save to AsyncStorage" };
+    }
+
+};
+
+// load
+export const get = async (key) => {
+    console.log("LOADING FROM ASYNCSTORAGE ::: ", key);
+
+    try {
+        const data = await AsyncStorage.getItem(key);
+        return JSON.parse(data);
+    } catch (e) {
+        return { error: "Failed to load from AsyncStorage" };
+    }
+};
+
+// clear
+export const clear = async () => {
+    console.log("ClEARING DATA FROM ASYNCSTORAGE");
+
+    try {
+        return await AsyncStorage.clear();
+    } catch (e) {
+        return { error: "Failed to clear AsyncStorage" };
+    }
+};
